871683b4b3d115679480399432f9a1cfa10d03ae
jianzhi-1 <jianzhi@berkeley.edu>
2024-11-07 10:15:19 -0800

    updated models - support multiple encoder layers, use nan instead of -inf, nan_to_num etc

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from utils import MelSpectrogramDataset, train
 import torch
 import torch.nn as nn
 import logging
+from collections import OrderedDict
 
 class EmotionModel(nn.Module):
     def transform(self, batch):
@@ -13,28 +14,17 @@ class EmotionModel(nn.Module):
 
         predicted_mel = self.transform(batch)
         assert predicted_mel.shape == (batch_size, seq_length, mels_dim)
-        predicted_mel = torch.where(torch.isneginf(batch["ai_mel"]), torch.tensor(0.0), predicted_mel) # purge tensor of -inf
+        predicted_mel = torch.nan_to_num(batch["ai_mel"], nan=0.0) # purge tensor of -inf
         assert predicted_mel.shape == (batch_size, seq_length, mels_dim)
-        assert not torch.any(torch.isneginf(predicted_mel))
-        assert not torch.any(torch.isinf(predicted_mel))
         assert not torch.any(torch.isnan(predicted_mel))
 
         target_mel = batch["data_mel"]
         assert target_mel.shape == (batch_size, seq_length, mels_dim)
-        target_mel = torch.where(torch.isneginf(batch["data_mel"]), torch.tensor(0.0), target_mel) # purge tensor of -inf
+        target_mel = torch.nan_to_num(batch["data_mel"], nan=0.0) # purge tensor of -inf
         assert target_mel.shape == (batch_size, seq_length, mels_dim)
-        assert not torch.any(torch.isneginf(target_mel))
-        assert not torch.any(torch.isinf(target_mel))
         assert not torch.any(torch.isnan(target_mel))
         assert mels_dim >= 1
-        cur_loss = torch.sum((predicted_mel - target_mel)**2, dim=2)/mels_dim # average MSE across mel dim = 80
-        assert cur_loss.shape == (batch_size, seq_length)
-        assert torch.all(batch["sequence_length"] > 0), "sequence length must be positive"
-
-        cur_loss_2 = torch.sum(cur_loss, dim=1)/batch["sequence_length"] # average MSE across the sequence
-        assert cur_loss_2.shape == (batch_size,)
-
-        loss = torch.sum(cur_loss_2)
+        loss = torch.sum((predicted_mel - target_mel)**2)
         return loss
   
     def get_validation_metric(self, validation_dataset, batch_size=64):
@@ -47,36 +37,9 @@ class EmotionModel(nn.Module):
         total = 0
         with torch.no_grad():
             for i, batch in enumerate(data_loader):
-                batch_size, seq_length, mels_dim = batch["ai_mel"].shape
-                assert batch["data_mel"].shape == (batch_size, seq_length, mels_dim)
-                
-                predicted_mel = self.transform(batch)
-                assert predicted_mel.shape == (batch_size, seq_length, mels_dim)
-                
-                predicted_mel = torch.where(torch.isneginf(batch["ai_mel"]), torch.tensor(0.0), predicted_mel) # purge tensor of -inf
-                assert predicted_mel.shape == (batch_size, seq_length, mels_dim)
-                assert not torch.any(torch.isneginf(predicted_mel))
-                assert not torch.any(torch.isinf(predicted_mel))
-                assert not torch.any(torch.isnan(predicted_mel))
-                
-                target_mel = batch["data_mel"]
-                assert target_mel.shape == (batch_size, seq_length, mels_dim)
-                target_mel = torch.where(torch.isneginf(batch["data_mel"]), torch.tensor(0.0), target_mel) # purge tensor of -inf
-                assert target_mel.shape == (batch_size, seq_length, mels_dim)
-                assert not torch.any(torch.isneginf(target_mel))
-                assert not torch.any(torch.isinf(target_mel))
-                assert not torch.any(torch.isnan(target_mel))
-                
-                cur_loss = torch.sum((predicted_mel - target_mel)**2, dim=2)/mels_dim # average MSE across mel dim = 80
-                assert cur_loss.shape == (batch_size, seq_length)
-                assert torch.all(batch["sequence_length"] > 0), "sequence length must be positive"
-                
-                cur_loss_2 = torch.sum(cur_loss, dim=1)/batch["sequence_length"] # average MSE across the sequence
-                assert cur_loss_2.shape == (batch_size,)
-                loss = torch.sum(cur_loss_2)
-                
+                loss = self.compute_loss(batch)
                 total_mse += loss
-                total += batch_size
+                total += batch["ai_mel"].size(0)
                 logging.info(f"validation; batch={i}; loss={loss.item()}; total_mse={total_mse}; ave_loss={total_mse/total}")
 
         return total_mse/total
@@ -106,41 +69,42 @@ class BaselineModel(EmotionModel):
         return after_linear_2
 
 class AddPositionalEncoding(nn.Module):
-    def __init__(self, d_model=256, input_dropout=0.1, timing_dropout=0.1, max_len=1024):
+    def __init__(self, d_model=256, input_dropout=0.1, timing_dropout=0.1, max_len=2048):
         super().__init__()
+        self.max_len = max_len
         self.timing_table = nn.Parameter(torch.FloatTensor(max_len, d_model))
         nn.init.normal_(self.timing_table)
         self.input_dropout = nn.Dropout(input_dropout)
         self.timing_dropout = nn.Dropout(timing_dropout)
 
     def forward(self, x):
+        assert x.shape[1] < self.max_len
         x = self.input_dropout(x)
         timing = self.timing_table[None, :x.shape[1], :]
         timing = self.timing_dropout(timing)
         return x + timing
 
 class TransformerEmotionModel(EmotionModel):
-    def __init__(self, d_model=512):
+    def __init__(self, d_model=512, num_encoder_layers=6, dropout=0.1):
         super().__init__()
         self.n_mels = 80
         self.d_model = d_model
         self.add_timing = AddPositionalEncoding(d_model)
-        self.encoder_layer_1 = nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True, norm_first=False, dropout=0.1, dim_feedforward=512)
-        self.encoder_layer_2 = nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True, norm_first=False, dropout=0.1, dim_feedforward=512)
-        self.embedding_layer = nn.Embedding(11, self.d_model) # len(self.label_encoder) = 11
-        self.pre_projection_layer = nn.Linear(self.n_mels, self.d_model)
-        self.post_projection_layer = nn.Linear(self.d_model, self.n_mels)
-        # self.conv = nn.Conv2d(80, 80, 3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-        # self.layer_norm_layer = nn.LayerNorm(d_model)
+        self.num_encoder_layers = num_encoder_layers
+        encoder_ls = []
+        for _ in range(num_encoder_layers):
+            encoder_ls.append(nn.TransformerEncoderLayer(d_model=d_model, nhead=8, batch_first=True, norm_first=False, dropout=dropout, dim_feedforward=d_model))
+        self.encoder_layers = nn.ModuleList(encoder_ls)
+        self.embedding_layer = nn.Embedding(11, d_model) # len(self.label_encoder) = 11
+        self.pre_projection_layer = nn.Linear(self.n_mels, d_model)
+        self.post_projection_layer = nn.Linear(d_model, self.n_mels)
 
     def transform(self, batch):
         
         batch_size, seq_length, _ = batch["ai_mel"].shape
         assert batch["ai_mel"].shape == (batch_size, seq_length, self.n_mels)
         
-        batch_input = torch.where(torch.isneginf(batch["ai_mel"]), torch.tensor(0.0), batch["ai_mel"])
-        assert not torch.any(torch.isneginf(batch_input))
-        assert not torch.any(torch.isinf(batch_input))
+        batch_input = torch.nan_to_num(batch["ai_mel"], nan=0.0)
         assert not torch.any(torch.isnan(batch_input))
         assert batch_input.shape == (batch_size, seq_length, self.n_mels)
 
@@ -165,27 +129,17 @@ class TransformerEmotionModel(EmotionModel):
         adjoined_with_timing = self.add_timing(adjoined)
         assert adjoined_with_timing.shape == (batch_size, 1 + seq_length, self.d_model)
         
-        after_encoder_1 = self.encoder_layer_1(
-            adjoined_with_timing, 
-            src_key_padding_mask=mask
-        ) # what is is_causal doing?
+        after_encoder = adjoined_with_timing
         
-        assert after_encoder_1.shape == (batch_size, 1 + seq_length, self.d_model)
+        for i in range(self.num_encoder_layers):
+            after_encoder = self.encoder_layers[i](after_encoder, src_key_padding_mask=mask)
+            assert after_encoder.shape == (batch_size, 1 + seq_length, self.d_model)
         
-        after_encoder_2 = self.encoder_layer_2(
-            after_encoder_1, 
-            src_key_padding_mask=mask
-        ) # what is is_causal doing?
-        
-        assert after_encoder_2.shape == (batch_size, 1 + seq_length, self.d_model)
-        
-        post_adjoined = self.post_projection_layer(after_encoder_2)
+        post_adjoined = self.post_projection_layer(after_encoder)
         assert post_adjoined.shape == (batch_size, 1 + seq_length, self.n_mels)
         
         res = post_adjoined[:,1:,:]
         assert res.shape == (batch_size, seq_length, self.n_mels)
-        assert not torch.any(torch.isneginf(res))
-        assert not torch.any(torch.isinf(res))
         assert not torch.any(torch.isnan(res))
         
         return res
