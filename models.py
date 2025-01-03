ccaaf46bb2a964f73c75e9e61f0ab375c074cbed
jianzhi-1 <jianzhi@berkeley.edu>
2025-01-03 01:36:18 -0800

    rescued gan, sdtw models

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
+import logging
+
+from soft_dtw_cuda import SoftDTW
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import logging
 
 class EmotionModel(nn.Module):
     def __init__(self):
@@ -144,6 +147,8 @@ class TransformerEmotionModel(EmotionModel):
         
         return res
 
+### === CNN Transformer models ===
+
 class ConvTransformerModel(EmotionModel):
     def __init__(self, d_model=512, num_encoder_layers=6, dropout=0.1):
         super().__init__()
@@ -250,6 +255,189 @@ class ConvTransformerModel(EmotionModel):
 
         padded_input = F.pad(batch_input, (2, 2, 2, 2), mode="reflect")
         return F.conv2d(padded_input.unsqueeze(1), torch.ones(1, 1, 5, 5).to(self.device) / 25.0, groups=batch_input.size(0)).squeeze(1) + output_tensor # residual network
+
+### === GAN model ===
+
+class Discriminator(nn.Module):
+
+    """
+    A CNN-based discriminator
+    The unique aspect is that it must classify variable length image.
+    """
+
+    def __init__(self, n_mels=80):
+        super(Discriminator, self).__init__()
+        self.n_mels = n_mels
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),  # Output: B x 32 x W x 80
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),  # Output: B x 32 x (W//2) x 40
+            nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),  # Output: B x 64 x (W//2) x 40
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),  # Output: B x 64 x (W//4) x 20
+        )
+        
+        # Standardise output
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 20))  # Output: B x 64 x 1 x 20
+        
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),  # Output: B x (64x20)
+            nn.Linear(64 * 20, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),  # Logit
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        batch_size, seq_length, _ = x.shape
+        assert x.shape == (batch_size, seq_length, self.n_mels) # x shape: B x W x 80
+        
+        x = x.unsqueeze(1)  # Add channel dimension 
+        assert x.shape == (batch_size, 1, seq_length, self.n_mels) # x shape: B x 1 x W x 80
+
+        x = self.conv_layers(x)
+        x = self.adaptive_pool(x)  # Standardise dimensions
+        assert x.shape == (batch_size, 64, 1, 20)
+
+        x = self.fc_layers(x)
+        return x
+
+class GAN():
+
+    """
+    Generative adversarial network class
+    Couples a generator and a discriminator and computes losses
+    """
+
+    def __init__(self, generator, discriminator, multiplier):
+        self.generator = generator
+        self.discriminator = discriminator
+        self.multiplier = multiplier
+
+    def compute_loss(self, batch):
+        batch_size, seq_length, mels_dim = batch["ai_mel"].shape
+        assert batch["data_mel"].shape == (batch_size, seq_length, mels_dim)
+
+        feature_loss, fake_mel = self.generator.compute_loss_gan(batch) # Optimised so that the prediction only happens once
+        human_mel = batch["data_mel"]
+
+        fake_probs = self.discriminator(fake_mel)
+        real_probs = self.discriminator(human_mel)
+
+        generator_loss = feature_loss + self.multiplier * torch.sum((fake_probs - 1)**2)/batch_size # L(G; D) = E[(D(G(s)) - 1)**2]
+
+        discriminator_loss = (torch.sum((real_probs - 1)**2) + torch.sum(fake_probs**2))/batch_size # L(D; G) = E[(D(x) - 1)**2 + D(G(s))**2]
+
+        return generator_loss, discriminator_loss
+
+    def get_validation_metric(self, data_loader, batch_size=64):
+
+        self.generator.eval()
+        self.discriminator.eval()
+
+        total_loss = 0.0
+        total = 0
+        with torch.no_grad():
+            for i, batch in enumerate(data_loader):
+                loss_g, _ = self.compute_loss(batch)
+                total_loss += loss_g
+                total += batch["ai_mel"].size(0)
+                logging.info(f"validation; batch={i}; loss={loss_g.item()}; total_mse={total_loss}; ave_loss={total_loss/total}")
+
+        return total_loss/total
+
+### === Soft DTW model === 
+
+class EmotionSDTWModel(EmotionModel):
+
+    def __init__(self):
+        super().__init__()
+        self.sdtw = SoftDTW(use_cuda=False, gamma=0.1)
+        self.multiplier = 0.2
+
+    def compute_loss(self, batch):
+        batch_size, seq_length, mels_dim = batch["ai_mel"].shape
+        assert batch["data_mel"].shape == (batch_size, seq_length, mels_dim)
+
+        predicted_mel = self.transform(batch)
+        assert predicted_mel.shape == (batch_size, seq_length, mels_dim)
+
+        target_mel = batch["data_mel"]
+        assert target_mel.shape == (batch_size, seq_length, mels_dim)
+
+        assert mels_dim == 80
+
+        feature_loss = torch.sum((predicted_mel - target_mel)**2)
+        dtw_loss = torch.sum(self.sdtw(predicted_mel, batch["original_data_mel"])) # torch.sum(self.sdtw(predicted_mel, target_mel)) # add DTW loss
+
+        loss = feature_loss + self.multiplier*dtw_loss # dtw_loss
+        return loss
+
+class TransformerSDTWEmotionModel(EmotionSDTWModel):
+    def __init__(self, d_model=512, num_encoder_layers=6, dropout=0.1):
+        super().__init__()
+        self.n_mels = 80
+        self.d_model = d_model
+        self.add_timing = AddPositionalEncoding(d_model)
+        self.num_encoder_layers = num_encoder_layers
+        encoder_ls = []
+        for _ in range(num_encoder_layers):
+            encoder_ls.append(nn.TransformerEncoderLayer(d_model=d_model, nhead=8, batch_first=True, norm_first=False, dropout=dropout, dim_feedforward=d_model))
+        self.encoder_layers = nn.ModuleList(encoder_ls)
+        self.embedding_layer = nn.Embedding(11, d_model) # len(self.label_encoder) = 11
+        self.pre_projection_layer = nn.Linear(self.n_mels, d_model)
+        self.post_projection_layer = nn.Linear(d_model, self.n_mels)
+
+    def transform(self, batch):
+        
+        batch_size, seq_length, _ = batch["ai_mel"].shape
+        assert batch["ai_mel"].shape == (batch_size, seq_length, self.n_mels)
+        
+        batch_input = batch["ai_mel"]
+        assert batch_input.shape == (batch_size, seq_length, self.n_mels)
+        assert not torch.any(torch.isnan(batch_input))
+
+        label = batch["labels"]
+        mask = batch["mask"]
+        
+        assert mask.shape == (batch_size, seq_length)
+        assert not torch.any(torch.isnan(mask))
+        mask = torch.cat((torch.full((batch_size, 1), False).to(self.device), mask), 1)
+        assert mask.shape == (batch_size, 1 + seq_length)
+        
+        assert label.shape == (batch_size,)
+        label_embedded = self.embedding_layer(label).unsqueeze(1)
+        assert label_embedded.shape == (batch_size, 1, self.d_model)
+        assert not torch.any(torch.isnan(label_embedded))
+        
+        pre_adjoined = self.pre_projection_layer(batch_input)
+        assert pre_adjoined.shape == (batch_size, seq_length, self.d_model)
+        assert not torch.any(torch.isnan(pre_adjoined))
+        
+        adjoined = torch.cat((label_embedded, pre_adjoined), 1)
+        assert adjoined.shape == (batch_size, 1 + seq_length, self.d_model)
+        assert not torch.any(torch.isnan(adjoined))
+        
+        adjoined_with_timing = self.add_timing(adjoined, mask)
+        assert adjoined_with_timing.shape == (batch_size, 1 + seq_length, self.d_model)
+        assert not torch.any(torch.isnan(adjoined_with_timing))
+        
+        after_encoder = adjoined_with_timing
+        
+        for i in range(self.num_encoder_layers):
+            after_encoder = self.encoder_layers[i](after_encoder, src_key_padding_mask=mask)
+            assert after_encoder.shape == (batch_size, 1 + seq_length, self.d_model)
+            assert not torch.any(torch.isnan(after_encoder))
+        
+        post_adjoined = self.post_projection_layer(after_encoder)
+        assert post_adjoined.shape == (batch_size, 1 + seq_length, self.n_mels)
+        assert not torch.any(torch.isnan(post_adjoined))
+        
+        res = post_adjoined[:,1:,:]
+        assert res.shape == (batch_size, seq_length, self.n_mels)
+        assert not torch.any(torch.isnan(res))
+        
+        return res
 
 ### === SPARC models and variants === 
 
